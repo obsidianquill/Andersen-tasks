1df86fc603d3288346898eb09e1d049822ce78ca
obsidianquill <58429507+obsidianquill@users.noreply.github.com>
2022-11-02 11:50:37 +0300

    Delete task3.js

--- task3.js
+++
@@ -1,9 +0,0 @@
-function displayArray(a) {
-    for(let i = 0; i < a.length; i++) {
-        if((a[i] % 3) === 0) {
-            console.log(a[i])
-        }
-    }
-}
-let arr = [1, 2, 3, 6];
-displayArray(arr);
